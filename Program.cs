36a1ed900fd48395711027e1736f713b020a2b4c
Alex Wolf <alexrudiwolf@gmail.com>
2024-10-06 10:32:27 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using MudBlazor.Services;
+using ChatApp.Components;
+using OpenAI;
+using System.Reflection.Metadata.Ecma335;
+using Azure.AI.OpenAI;
+using Azure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
